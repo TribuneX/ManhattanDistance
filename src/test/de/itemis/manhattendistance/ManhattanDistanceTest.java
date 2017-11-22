8575a8c3d395deed9961ef5ba5795e5d0ec9e0b3
TribuneX <saschableidner@gmail.com>
2017-11-22 16:58:36 +0100

    Upgrade to JUnit 5.0.2

--- src/test/de/itemis/manhattendistance/ManhattanDistanceTest.java
+++ src/test/de/itemis/manhattendistance/ManhattanDistanceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ObjectArrayArguments;
 
 import java.util.stream.Stream;
 
@@ -16,15 +15,15 @@ public class ManhattanDistanceTest {
 
     static Stream<Arguments> intArguments() {
         return Stream.of(
-                ObjectArrayArguments.create(1, 1, 2, 2, 2),
-                ObjectArrayArguments.create(1, 1, 1, 1, 0)
+                Arguments.of(1, 1, 2, 2, 2),
+                Arguments.of(1, 1, 1, 1, 0)
         );
     }
 
-    static Stream<Arguments> classArguments() {
+    static Stream<Arguments> objectArguments() {
         return Stream.of(
-                ObjectArrayArguments.create(new Point(1, 1), new Point(2, 2), 2),
-                ObjectArrayArguments.create(new Point(2, 2), new Point(5, 7), 8)
+                Arguments.of(new Point(1, 1), new Point(2, 2), 2),
+                Arguments.of(new Point(2, 2), new Point(5, 7), 8)
         );
     }
 
@@ -32,19 +31,18 @@ public class ManhattanDistanceTest {
     // Inspired by http://code-mentor.com/blog/2017/junit5-parameterized-tests.html
     @ParameterizedTest
     @DisplayName("Test Distance with Integer")
-    @MethodSource(names = "intArguments")
-    void distanceCalculation(int x1, int y1, int x2, int y2, int expected) {
+    @MethodSource("intArguments")
+    void checkDistanceCalcForTwoPoints(int x1, int y1, int x2, int y2, int expected) {
         Point p1 = new Point(x1, y1);
         Point p2 = new Point(x2, y2);
         ManhattanDistance manhattanDistance = new ManhattanDistance();
-
         assertEquals(expected, manhattanDistance.measureDistance(p1, p2));
     }
 
     @ParameterizedTest
     @DisplayName("Test Distance with Point objects")
-    @MethodSource(names = "classArguments")
-    void distanceCalculation(Point p1, Point p2, int expected) {
+    @MethodSource("objectArguments")
+    void checkDistanceCalcForTwoPoints(Point p1, Point p2, int expected) {
         ManhattanDistance manhattanDistance = new ManhattanDistance();
 
         assertEquals(expected, manhattanDistance.measureDistance(p1, p2));
@@ -55,7 +53,6 @@ public class ManhattanDistanceTest {
         Point p1 = new Point(1, 1);
         Point p2 = new Point(1, 1);
         ManhattanDistance manhattanDistance = new ManhattanDistance();
-
         assertEquals(0, manhattanDistance.measureDistance(p1, p2));
     }
 
